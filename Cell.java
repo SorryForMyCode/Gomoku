faac8c16d466d540e4eca92acc08fd475704c288
SorryForMyCode <Rear-guard0@mail.ru>
2018-07-08 15:50:50 +0700

    package

--- Cell.java
+++ Cell.java
@@ -1,49 +1,49 @@
 package Gomoku;
 
-public class Cell {
+class Cell {
     private int x;
     private int y;
     private char figure;
 
-    public Cell(int x, int y, char figure) {
+    Cell(int x, int y, char figure) {
         this.x = x;
         this.y = y;
         this.figure = figure;
     }
 
-    public Cell() {
+    Cell() {
         x = -1;
         y = -1;
         figure = '.';
     }
 
-    public boolean isValid(){
+    boolean isValid(){
         return x >= 0 && x <= 18 &&
                 y >= 0 && y <= 18 &&
                 figure == '.';
     }
 
-    public int getX() {
+    int getX() {
         return x;
     }
 
-    public int getY() {
+    int getY() {
         return y;
     }
 
-    public void setX(int x) {
+    void setX(int x) {
         this.x = x;
     }
 
-    public void setY(int y) {
+    void setY(int y) {
         this.y = y;
     }
 
-    public char getFigure() {
+    char getFigure() {
         return figure;
     }
 
-    public void setFigure(char figure) {
+    void setFigure(char figure) {
         this.figure = figure;
     }
 
