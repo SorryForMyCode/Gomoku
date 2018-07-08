5c18fd85e646ff506e43497c35b8600b06130fee
SorryForMyCode <Rear-guard0@mail.ru>
2018-07-08 17:06:52 +0700

    to size

--- Board.java
+++ Board.java
@@ -43,29 +43,27 @@ class Board {
     }
 
     private boolean checkDiagonal(char figure){
-        for(int i = 0; i < desk.length; i++)
+        for(int i = 0; i < size; i++)
             if(leftToRightDiagonal(i, 0, figure)) return true;
 
-        for (int i = 1; i < desk.length; i++)
+        for (int i = 1; i < size; i++)
             if(leftToRightDiagonal(0, i, figure)) return true;
 
-        for (int i = 0; i < desk.length; i++)
+        for (int i = 0; i < size; i++)
             if(rightToLeftDiagonal(i, 0, figure)) return true;
 
-        for (int i = 1; i < desk.length; i++)
-            if(rightToLeftDiagonal(desk.length - 1, i, figure)) return true;
+        for (int i = 1; i < size; i++)
+            if(rightToLeftDiagonal(size - 1, i, figure)) return true;
 
         return false;
     }
 
     private boolean leftToRightDiagonal(int x, int y, char figure){
         int count = 0, maxCount = 0;
-        for (int i = y, j = x; i < desk.length && j < desk.length; i++, j++) {
+        for (int i = y, j = x; i < size && j < size; i++, j++) {
             if(desk[i][j].getFigure() == figure) count++;
-            if(desk[i][j].getFigure() != figure &&
-                    count != 0 ||
-                    i == desk.length - 1 ||
-                    j == desk.length - 1){
+
+            if(desk[i][j].getFigure() != figure && count != 0 || i == size - 1 || j == size - 1){
                 if(count > maxCount) maxCount = count;
                 count = 0;
             }
@@ -75,11 +73,11 @@ class Board {
 
     private boolean rightToLeftDiagonal(int x, int y, char figure){
         int count = 0, maxCount = 0;
-        for (int i = y, j = x; i < desk.length && j >= 0; i++, j--) {
+        for (int i = y, j = x; i < size && j >= 0; i++, j--) {
             if(desk[i][j].getFigure() == figure) count++;
             if(desk[i][j].getFigure() != figure &&
                     count != 0 ||
-                    i == desk.length - 1 ||
+                    i == size - 1 ||
                     j == 0){
                 if(count > maxCount) maxCount = count;
                 count = 0;
@@ -90,10 +88,10 @@ class Board {
 
     private boolean checkVertical(char figure){
         int count = 0 , maxCount = 0;
-        for(int i = 0; i < desk.length; i++){ //2coord
-            for (int j = 0; j < desk.length; j++) { //1coord
+        for(int i = 0; i < size; i++){ //2coord
+            for (int j = 0; j < size; j++) { //1coord
                 if(desk[i][j].getFigure() == figure) count++;
-                if(desk[i][j].getFigure() != figure && count != 0 || j == desk.length - 1){
+                if(desk[i][j].getFigure() != figure && count != 0 || j == size - 1){
                     if(count > maxCount) maxCount = count;
                     count = 0;
                 }
@@ -104,10 +102,10 @@ class Board {
 
     private boolean checkHorizontal(char figure){
         int count = 0 , maxCount = 0;
-        for(int i = 0; i < desk.length; i++){ //1coord
-            for (int j = 0; j < desk.length; j++) { //2coord
+        for(int i = 0; i < size; i++){ //1coord
+            for (int j = 0; j < size; j++) { //2coord
                 if(desk[i][j].getFigure() == figure) count++;
-                if(desk[i][j].getFigure() != figure && count != 0 || j == desk.length - 1){
+                if(desk[i][j].getFigure() != figure && count != 0 || j == size - 1){
                     if(count > maxCount) maxCount = count;
                     count = 0;
                 }
